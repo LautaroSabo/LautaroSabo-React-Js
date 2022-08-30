e0fb7a6696686bb680c71d7fb4bda9e7984c2474
Lautaro Sabo <Lautysabo6@gmail.com>
2022-08-30 20:53:17 -0300

    Trabajo de la clase 3

--- trabajos/src/App.js
+++ trabajos/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import'./App.css';
+import Navbar from './components/NavBar/Index';
 
 function App() {
+
   return (
+    <>
+    <Navbar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lesa
-        </a>
-      </header>
+      {/*estilos en linea*/}
+      <p
+      >Bienvenidos a mi Página!
+      </p>
     </div>
+    </>
   );
 }
 
---
+++ trabajos/src/components/NavBar/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Styles.css'
+
+const Navbar = () => {
+    return (
+        <div className='Barra'>
+            <h1>Lesa</h1>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
